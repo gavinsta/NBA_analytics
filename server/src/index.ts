738ac07cf004ed3e67e26e33aec4d960a0294a79
Gavinsta <gavin.kw.lau@gmail.com>
2022-11-22 13:47:51 -0700

    Creating server to serve React app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,22 @@
-import express from "express";
-const PORT = 8080;
-const app = express();
-app.get("/", (req, res) => {
-  return res.send("hello world");
+
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import path from "path";
+import cors from "cors";
+
+dotenv.config();
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('<h1>Hello World From the Typescript Server!</h1>')
 });
-app.listen(PORT, () => {
-  console.log("Listening on port: " + PORT);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 });
